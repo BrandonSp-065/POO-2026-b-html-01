108adf79a62756ee0b1da947916dac4c2db3baba
Brandon Sanchez <20253ds034@utez.edu.mx>
2026-05-29 14:30:26 -0600

    [UPDATE] practica completa

---
+++ scripct.js
@@ -0,0 +1,24 @@
+const btnSaludar = document.getElementById('btn-saludar');
+let mensajeMostrado=false;
+
+console.log(btnSaludar);
+
+btnSaludar.addEventListener('click', function(){
+
+    if(!mensajeMostrado){
+    const seccionContacto=document.getElementById('contacto');
+    console.log('click')
+
+    const mensaje = document.createElement ('p');
+    mensaje.textContent = 'Gracias por tu visita';
+    mensaje.style.color='#27ae60';
+    mensaje.style.fontWeight= 'bold';
+
+    seccionContacto.appendChild(mensaje);
+    mensajeMostrado = true;
+    btnSaludar.textContent = "Mensaje Enviado";
+    btnSaludar.disabled = true;
+    btnSaludar.style.backgroundColor = '#bdc3c7';
+    }
+
+});
